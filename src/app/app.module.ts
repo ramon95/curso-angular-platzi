f07331480a0c83320f9ba5bb8d086e1a23e52704
Ramon Perez <ramon.a.p.r.95@gmail.com>
2022-03-28 12:49:27 -0500

    Limpiando el repo para el curso de apis rest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { AppRoutingModule } from './app-routing.module';
 import { ImgComponent } from './components/img/img.component';
+import { NavComponent } from './components/nav/nav.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
-import { NavComponent } from './components/nav/nav.component';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { TimeAgoPipe } from './pipes/time-ago.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -20,9 +18,7 @@ import { HighlightDirective } from './directives/highlight.directive';
     ProductComponent,
     ProductsComponent,
     NavComponent,
-    ReversePipe,
     TimeAgoPipe,
-    HighlightDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
